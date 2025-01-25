2a15040410aff1106b6246ed3ec6bffa2d1f87b9
Kamil <elzyr11@gmail.com>
2025-01-25 15:19:25 +0100

    a

--- administrator.sql
+++ administrator.sql
@@ -3,7 +3,6 @@ CREATE OR REPLACE PACKAGE admin_seanse AS
 
     -- Procedura dodaj¹ca nowy film
     PROCEDURE add_film(
-        p_film_id       IN NUMBER,
         p_tytul         IN VARCHAR2,
         p_czas_trwania  IN NUMBER, -- w minutach
         p_minimalny_wiek IN NUMBER,
@@ -12,23 +11,184 @@ CREATE OR REPLACE PACKAGE admin_seanse AS
 
     -- Procedura dodaj¹ca nowy seans
     PROCEDURE add_seans(
-        p_repertuar_id     IN NUMBER,
         p_film_id          IN NUMBER,
         p_sala_id          IN NUMBER,
         p_data_rozpoczecia IN DATE
     );
 
-    -- Definicja wyj¹tków
-    EXCEPTION
-        e_film_exists               WHEN OTHERS THEN;
-        e_invalid_time_range         -- Jeœli czas seansu jest poza dozwolonym zakresem
-            EXCEPTION;
-        e_overlap_seans              -- Jeœli seans nachodzi na inny seans
-            EXCEPTION;
-        e_sala_not_found             -- Jeœli sala nie istnieje
-            EXCEPTION;
-        e_film_not_found             -- Jeœli film nie istnieje
-            EXCEPTION;
+    -- Procedura dodaj¹ca now¹ salê
+    PROCEDURE add_sala(
+        p_nazwa           IN VARCHAR2,
+        p_ilosc_rzedow    IN NUMBER,
+        p_miejsca_w_rzedzie IN NUMBER
+    );
+
+    -- Procedura dodaj¹ca now¹ kategoriê
+    PROCEDURE add_kategoria(
+        p_nazwa           IN VARCHAR2
+    );
 
 END admin_seanse;
 /
+
+-- Implementacja Pakietu admin_seanse
+CREATE OR REPLACE PACKAGE BODY admin_seanse AS
+
+    -- Procedura dodaj¹ca nowy film
+    PROCEDURE add_film(
+        p_tytul         IN VARCHAR2,
+        p_czas_trwania  IN NUMBER,
+        p_minimalny_wiek IN NUMBER,
+        p_kategoria_id  IN NUMBER
+    ) IS
+        v_kategoria_ref REF Kategoria;
+    BEGIN
+        -- Sprawdzenie czy kategoria istnieje
+        SELECT REF(k)
+        INTO v_kategoria_ref
+        FROM Kategoria_table k
+        WHERE k.kategoria_id = p_kategoria_id;
+
+        -- Wstawienie nowego filmu (id generowane automatycznie przez sekwencjê)
+        INSERT INTO Film_table (
+            tytul,
+            czas_trwania,
+            minimalny_wiek,
+            kategoria_ref
+        ) VALUES (
+            p_tytul,
+            p_czas_trwania,
+            p_minimalny_wiek,
+            v_kategoria_ref
+        );
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20001, 'B³¹d podczas dodawania filmu. Upewnij siê, ¿e kategoria istnieje.');
+    END add_film;
+
+    -- Procedura dodaj¹ca nowy seans
+    PROCEDURE add_seans(
+        p_film_id          IN NUMBER,
+        p_sala_id          IN NUMBER,
+        p_data_rozpoczecia IN DATE
+    ) IS
+        v_film_ref       REF Film;
+        v_sala_ref       REF Sala;
+        v_czas_trwania   NUMBER;
+        v_data_zakonczenia DATE;
+        v_existing_seans_count NUMBER;
+    BEGIN
+        -- Pobranie referencji do filmu
+        SELECT REF(f)
+        INTO v_film_ref
+        FROM Film_table f
+        WHERE f.film_id = p_film_id;
+
+        -- Pobranie referencji do sali
+        SELECT REF(s)
+        INTO v_sala_ref
+        FROM Sala_table s
+        WHERE s.sala_id = p_sala_id;
+
+        -- Pobranie czasu trwania filmu
+        SELECT f.czas_trwania
+        INTO v_czas_trwania
+        FROM Film_table f
+        WHERE f.film_id = p_film_id;
+
+        -- Obliczenie czasu zakoñczenia seansu
+        v_data_zakonczenia := p_data_rozpoczecia + NUMTODSINTERVAL(v_czas_trwania + 30, 'MINUTE');
+
+        -- Sprawdzenie godzin rozpoczêcia i zakoñczenia seansu
+        IF TO_CHAR(p_data_rozpoczecia, 'HH24:MI') < '07:00' OR TO_CHAR(p_data_rozpoczecia, 'HH24:MI') > '22:00' THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Godzina rozpoczêcia seansu musi byæ miêdzy 07:00 a 22:00.');
+        END IF;
+
+        IF TO_CHAR(v_data_zakonczenia, 'HH24:MI') > '23:59' THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Seans nie mo¿e koñczyæ siê po pó³nocy.');
+        END IF;
+
+        -- Sprawdzenie przerwy 30 minut miêdzy seansami w tej samej sali
+        SELECT COUNT(*)
+        INTO v_existing_seans_count
+        FROM Repertuar_table r
+        WHERE r.sala_ref = v_sala_ref
+          AND (
+                (p_data_rozpoczecia BETWEEN r.data_rozpoczecia AND (r.data_rozpoczecia + NUMTODSINTERVAL((SELECT f.czas_trwania FROM Film_table f WHERE REF(f) = r.film_ref) + 30, 'MINUTE')))
+                OR
+                (v_data_zakonczenia BETWEEN r.data_rozpoczecia AND (r.data_rozpoczecia + NUMTODSINTERVAL((SELECT f.czas_trwania FROM Film_table f WHERE REF(f) = r.film_ref) + 30, 'MINUTE')))
+                OR
+                (r.data_rozpoczecia BETWEEN p_data_rozpoczecia AND v_data_zakonczenia)
+              );
+
+
+        IF v_existing_seans_count > 0 THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Nowy seans koliduje z istniej¹cymi seansami w tej sali.');
+        END IF;
+
+        -- Wstawienie nowego seansu (id generowane automatycznie przez sekwencjê)
+        INSERT INTO Repertuar_table (
+            film_ref,
+            sala_ref,
+            data_rozpoczecia
+        ) VALUES (
+            v_film_ref,
+            v_sala_ref,
+            p_data_rozpoczecia
+        );
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20004, 'B³¹d podczas dodawania seansu. SprawdŸ dane wejœciowe.');
+    END add_seans;
+
+    -- Procedura dodaj¹ca now¹ salê
+    PROCEDURE add_sala(
+        p_nazwa           IN VARCHAR2,
+        p_ilosc_rzedow    IN NUMBER,
+        p_miejsca_w_rzedzie IN NUMBER
+    ) IS
+        v_miejsca Miejsca_Typ := Miejsca_Typ();
+        v_miejsce_id NUMBER := 1;
+        v_rzad NUMBER;
+        v_miejsce NUMBER;
+    BEGIN
+        -- Tworzenie zagnie¿d¿onej tabeli miejsc
+        FOR v_rzad IN 1..p_ilosc_rzedow LOOP
+            FOR v_miejsce IN 1..p_miejsca_w_rzedzie LOOP
+                v_miejsca.EXTEND;
+                v_miejsca(v_miejsca.COUNT) := Miejsce(v_miejsce_id, v_rzad, v_miejsce, 0);
+                v_miejsce_id := v_miejsce_id + 1;
+            END LOOP;
+        END LOOP;
+
+        -- Wstawienie nowej sali z miejscami
+        INSERT INTO Sala_table (
+            nazwa,
+            miejsca
+        ) VALUES (
+            p_nazwa,
+            v_miejsca
+        );
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20005, 'B³¹d podczas dodawania sali. SprawdŸ dane wejœciowe.');
+    END add_sala;
+
+    -- Procedura dodaj¹ca now¹ kategoriê
+    PROCEDURE add_kategoria(
+        p_nazwa           IN VARCHAR2
+    ) IS
+    BEGIN
+        -- Wstawienie nowej kategorii
+        INSERT INTO Kategoria_table (
+            nazwa
+        ) VALUES (
+            p_nazwa
+        );
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20006, 'B³¹d podczas dodawania kategorii. SprawdŸ dane wejœciowe.');
+    END add_kategoria;
+
+END admin_seanse;
--- testowanie.sql
+++ testowanie.sql
@@ -1,595 +1,134 @@
 BEGIN
-    -- 1. Usuwanie Wyzwalaczy (Triggers)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TRIGGER trg_rezerwacja_data';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4080 THEN -- ORA-04080: trigger does not exist
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TRIGGER release_seat_on_cancel';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4080 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TRIGGER ensure_unique_seans_per_sala';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4080 THEN
-                RAISE;
-            END IF;
-    END;
+    -- Dodawanie kategorii
+    admin_seanse.add_kategoria('Komedia');
+    admin_seanse.add_kategoria('Horror');
+    admin_seanse.add_kategoria('Dramat');
+    admin_seanse.add_kategoria('Sci-Fi');
+    admin_seanse.add_kategoria('Fantasy');
 
-    -- 2. Usuwanie Pakietï¿½w i Ich Ciaï¿½ (Packages and Package Bodies)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE BODY Rezerwacja_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN -- ORA-04043: object does not exist
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE Rezerwacja_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE BODY AnulujRezerwacje_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE AnulujRezerwacje_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE BODY Repertuar_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP PACKAGE Repertuar_Pkg';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-
-    -- 3. Usuwanie Sekwencji (Sequences)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP SEQUENCE Bilet_SEQ';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -2289 THEN -- ORA-02289: sequence does not exist
-                RAISE;
-            END IF;
-    END;
-
-    -- 4. Usuwanie Tabel Obiektowych (Object Tables)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Rezerwacja_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN -- ORA-00942: table or view does not exist
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Bilet_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Repertuar_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Miejsce_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Sala_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Film_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Uzytkownik_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Kategoria_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TABLE Rola_table CASCADE CONSTRAINTS';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -942 THEN
-                RAISE;
-            END IF;
-    END;
-
-    -- 5. Usuwanie Typï¿½w Obiektowych (Object Types)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Rezerwacja FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN -- ORA-04043: object does not exist
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Bilet FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Repertuar FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Miejsce FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Sala FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Film FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Uzytkownik FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Kategoria FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Rola FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
-    
-    -- Usuwanie innych typï¿½w, jeï¿½li istniejï¿½ (np. Bilety_Typ)
-    BEGIN
-        EXECUTE IMMEDIATE 'DROP TYPE Bilety_Typ FORCE';
-    EXCEPTION
-        WHEN OTHERS THEN
-            IF SQLCODE != -4043 THEN
-                RAISE;
-            END IF;
-    END;
+    DBMS_OUTPUT.PUT_LINE('Kategorie zosta³y dodane.');
 END;
 /
 
-    
--- -------------------------------
--- Sekcja: Tworzenie typï¿½w obiektï¿½w
--- -------------------------------
-
--- Typ Kategoria
-CREATE OR REPLACE TYPE Kategoria AS OBJECT (
-    kategoria_id NUMBER,
-    nazwa VARCHAR2(100)
-);
-/
-
--- Typ Miejsce
-CREATE OR REPLACE TYPE Miejsce AS OBJECT (
-    miejsce_id NUMBER,
-    rzad NUMBER,
-    numer NUMBER,
-    czy_zajete NUMBER
-);
-/
-
-CREATE OR REPLACE TYPE Miejsca_Typ AS TABLE OF Miejsce;
-
--- Typ Sala
-CREATE OR REPLACE TYPE Sala AS OBJECT (
-    sala_id NUMBER,
-    nazwa VARCHAR2(50),
-    miejsca Miejsca_Typ
-);
-/
-
-CREATE OR REPLACE TYPE Uzytkownik AS OBJECT (
-    user_id    NUMBER,
-    imie       VARCHAR2(50),
-    nazwisko   VARCHAR2(50),
-    wiek       NUMBER,
-    email      VARCHAR2(100),
-    rola       VARCHAR2(50) -- zamiast rola_ref
-);
-/
-
--- Typ Film
-CREATE OR REPLACE TYPE Film AS OBJECT (
-    film_id NUMBER,
-    tytul VARCHAR2(200),
-    czas_trwania NUMBER,  -- w minutach
-    minimalny_wiek NUMBER,
-    kategoria_ref REF Kategoria
-);
-/
-
--- Typ Repertuar
-CREATE OR REPLACE TYPE Repertuar AS OBJECT (
-    repertuar_id NUMBER,
-    film_ref REF Film,
-    sala_ref REF Sala,
-    data_rozpoczecia DATE,
-    MEMBER FUNCTION ilosc_miejsc_zajetych RETURN NUMBER,
-    MEMBER FUNCTION data_zakonczenia RETURN DATE
-);
-/
-
--- Typ Bilet
-CREATE OR REPLACE TYPE Bilet AS OBJECT (
-    bilet_id NUMBER,
-    cena NUMBER(5,2),
-    seans_ref REF Repertuar,
-    rzad NUMBER,
-    miejsce NUMBER,
-    MEMBER FUNCTION data_seansu RETURN DATE
-);
-/
-
--- Typ Bilety_Typ
-CREATE OR REPLACE TYPE Bilety_Typ AS TABLE OF REF Bilet;
-/
-
--- Typ Rezerwacja
-CREATE OR REPLACE TYPE Rezerwacja AS OBJECT (
-    rezerwacja_id NUMBER,
-    data_rezerwacji DATE, 
-    cena_laczna NUMBER,
-    czy_anulowane NUMBER,  -- 0 - opï¿½acone, 1 - anulowane
-    repertuar_ref REF Repertuar,
-    uzytkownik_ref REF Uzytkownik,
-    bilety Bilety_Typ
-);
-/
-
-
--- -------------------------------
--- Sekcja: Tworzenie tabel
--- -------------------------------
-
--- Tabela Kategoria_table
-CREATE TABLE Kategoria_table OF Kategoria (
-    PRIMARY KEY (kategoria_id),
-    CONSTRAINT kategoria_nazwa_ck CHECK (nazwa IS NOT NULL)
-);
-/
-
-
--- Tabela Sala_table
-CREATE TABLE Sala_table OF Sala (
-    PRIMARY KEY (sala_id),
-    CONSTRAINT sala_nazwa_ck CHECK (nazwa IS NOT NULL)
-)NESTED TABLE miejsca STORE AS miejsca_nt;
-/
-    
-CREATE TABLE Miejsce_table OF Miejsce
-(
-  primary key (miejsce_id),
-  CONSTRAINT miejsce_rzad_ck CHECK (rzad > 0),
-  CONSTRAINT miejsce_numer_ck CHECK (numer > 0)
-)
-OBJECT IDENTIFIER IS PRIMARY KEY;
-
--- Tabela Uzytkownik_table
-CREATE TABLE Uzytkownik_table OF Uzytkownik (
-    PRIMARY KEY (user_id),
-    CONSTRAINT uzytkownik_email_unique UNIQUE(email),
-    CONSTRAINT uzytkownik_wiek_ck CHECK (wiek > 15),
-    CONSTRAINT uzytkownik_rola_ck CHECK (rola IN ('standard','premium'))
-);
-/
-
--- Tabela Film_table
-CREATE TABLE Film_table OF Film (
-    PRIMARY KEY (film_id),
-    CONSTRAINT film_czas_trwania_ck CHECK (czas_trwania > 0),
-    CONSTRAINT film_minimalny_wiek_ck CHECK (minimalny_wiek >= 0 AND minimalny_wiek <= 18)
-);
-/
-
--- Tabela Repertuar_table
-CREATE TABLE Repertuar_table OF Repertuar (
-    PRIMARY KEY (repertuar_id)
-);
-/
-
--- Tabela Bilet_table
-CREATE TABLE Bilet_table OF Bilet (
-    PRIMARY KEY (bilet_id),
-    SCOPE FOR (seans_ref) IS repertuar_table,
-    CONSTRAINT bilet_cena_ck CHECK (cena > 0)
-) OBJECT IDENTIFIER IS PRIMARY KEY;
-/
-
--- Tabela Rezerwacja_table
-CREATE TABLE Rezerwacja_table OF Rezerwacja (
-    PRIMARY KEY (rezerwacja_id),
-    SCOPE FOR(repertuar_ref) is repertuar_table,
-    SCOPE FOR(uzytkownik_ref) is uzytkownik_table,
-    CONSTRAINT rezerwacja_cena_laczna_ck CHECK (cena_laczna > 0),
-    CONSTRAINT rezerwacja_czy_anulowane_ck CHECK (czy_anulowane IN (0, 1))
-) OBJECT IDENTIFIER IS PRIMARY KEY
-NESTED TABLE bilety STORE AS bilety_nt;
-/
-
-
--- -------------------------------
--- Sekcja: Tworzenie ciaï¿½ typï¿½w obiektï¿½w
--- -------------------------------
-CREATE OR REPLACE TYPE BODY Repertuar AS
-    MEMBER FUNCTION ilosc_miejsc_zajetych RETURN NUMBER IS
-        v_ilosc NUMBER;
-        v_sala_id NUMBER;
-    BEGIN
-        -- Pobierz sala_id z referencji SELF.sala_ref
-        SELECT s.sala_id
-          INTO v_sala_id
-          FROM Sala_table s
-         WHERE REF(s) = SELF.sala_ref;
-
-        -- Zlicz zajï¿½te miejsca w zagnieï¿½dï¿½onej tabeli
-        SELECT COUNT(*)
-          INTO v_ilosc
-          FROM TABLE(
-              SELECT s.miejsca 
-              FROM Sala_table s
-              WHERE s.sala_id = v_sala_id
-          )
-         WHERE czy_zajete = 1;
-
-        RETURN v_ilosc;
-    END ilosc_miejsc_zajetych;
-
-    MEMBER FUNCTION data_zakonczenia RETURN DATE IS
-        v_czas_trwania NUMBER;
-    BEGIN
-        -- Pobierz czas trwania filmu
-        SELECT f.czas_trwania 
-          INTO v_czas_trwania
-          FROM Film_table f
-         WHERE REF(f) = SELF.film_ref;
-
-        -- Oblicz datï¿½ zakoï¿½czenia seansu
-        RETURN SELF.data_rozpoczecia + (v_czas_trwania / (24 * 60));
-    END data_zakonczenia;
-END;
-/
-
-CREATE OR REPLACE TYPE BODY Bilet AS
-    MEMBER FUNCTION data_seansu RETURN DATE IS
-        v_data DATE;
-    BEGIN
-        SELECT r.data_rozpoczecia 
-          INTO v_data
-          FROM Repertuar_table r
-         WHERE REF(r) = seans_ref;
-
-        RETURN v_data;
-    END data_seansu;
-END;
-/
-
-
--- -------------------------------
--- Sekcja: Tworzenie wyzwalaczy
--- -------------------------------
-
--- Wyzwalacz dla sprawdzenia daty rezerwacji
-CREATE OR REPLACE TRIGGER trg_rezerwacja_data
-BEFORE INSERT OR UPDATE ON Rezerwacja_table
-FOR EACH ROW
-DECLARE
-    v_data_rozpoczecia DATE;
 BEGIN
-    BEGIN
-        SELECT r.data_rozpoczecia 
-        INTO v_data_rozpoczecia
-        FROM Repertuar_table r
-        WHERE REF(r) = :NEW.repertuar_ref;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE_APPLICATION_ERROR(-20002, 'Nie znaleziono repertuaru dla wskazanej rezerwacji.');
-    END;
+    -- Dodawanie filmów
+    admin_seanse.add_film(
+        p_tytul => 'Wielka Przygoda',
+        p_czas_trwania => 120,
+        p_minimalny_wiek => 7,
+        p_kategoria_id => 1 -- ID kategorii 'Komedia'
+    );
 
-    -- Sprawdï¿½, czy data rezerwacji jest pï¿½niejsza niï¿½ rozpoczï¿½cie repertuaru
-    IF :NEW.data_rezerwacji > v_data_rozpoczecia THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Nie moï¿½na rezerwowaï¿½ biletï¿½w na seans, ktï¿½ry juï¿½ siï¿½ zaczï¿½ï¿½.');
-    END IF;
+    admin_seanse.add_film(
+        p_tytul => 'Strach w Nocy',
+        p_czas_trwania => 90,
+        p_minimalny_wiek => 16,
+        p_kategoria_id => 2 -- ID kategorii 'Horror'
+    );
+
+    admin_seanse.add_film(
+        p_tytul => 'Historia Mi³oœci',
+        p_czas_trwania => 130,
+        p_minimalny_wiek => 12,
+        p_kategoria_id => 3 -- ID kategorii 'Dramat'
+    );
+
+    admin_seanse.add_film(
+        p_tytul => 'Kosmiczna Odyseja',
+        p_czas_trwania => 150,
+        p_minimalny_wiek => 10,
+        p_kategoria_id => 4 -- ID kategorii 'Sci-Fi'
+    );
+
+    admin_seanse.add_film(
+        p_tytul => 'Magiczna Kraina',
+        p_czas_trwania => 110,
+        p_minimalny_wiek => 6,
+        p_kategoria_id => 5 -- ID kategorii 'Fantasy'
+    );
+
+    DBMS_OUTPUT.PUT_LINE('Filmy zosta³y dodane.');
 END;
 /
 
 
-        
--- Wyzwalacz na zwalnianie miejsc przy anulowaniu rezerwacji
-CREATE OR REPLACE TRIGGER release_seat_on_cancel
-AFTER UPDATE OF czy_anulowane ON Rezerwacja_table
-FOR EACH ROW
-WHEN (NEW.czy_anulowane = 1)
-DECLARE
-    v_sala_id NUMBER;
-    v_rzad NUMBER;
-    v_numer NUMBER;
+-- Poprawne przypadki
 BEGIN
-    -- Pobierz sala_id z powiÄ…zanego repertuaru
-    SELECT r.sala_ref.sala_id
-    INTO v_sala_id
-    FROM Repertuar_table r
-    WHERE REF(r) = :NEW.repertuar_ref;
-    
-    -- Iteruj po biletach i aktualizuj odpowiednie miejsca
-    FOR bilet_rec IN (
-        SELECT b.rzad, b.miejsce
-        FROM Bilet_table b
-        WHERE REF(b) IN (
-            SELECT COLUMN_VALUE 
-            FROM TABLE(:OLD.bilety)
-        )
-    ) LOOP
-        -- Oznacz miejsce jako wolne w nested table
-        UPDATE TABLE(
-            SELECT s.miejsca 
-            FROM Sala_table s 
-            WHERE s.sala_id = v_sala_id
-        ) m
-        SET m.czy_zajete = 0
-        WHERE m.rzad = bilet_rec.rzad AND m.numer = bilet_rec.miejsce;
-    END LOOP;
+    -- Dodaj salê
+    admin_seanse.add_sala(
+        p_nazwa => 'Sala 1',
+        p_ilosc_rzedow => 5,
+        p_miejsca_w_rzedzie => 10
+    );
+
+    -- Dodaj film
+    admin_seanse.add_film(
+        p_tytul => 'Film A',
+        p_czas_trwania => 120,
+        p_minimalny_wiek => 12,
+        p_kategoria_id => 1 -- Zak³adaj¹c, ¿e istnieje kategoria o ID 1
+    );
+
+    -- Dodaj seans
+    admin_seanse.add_seans(
+        p_film_id => 1, -- Zak³adaj¹c, ¿e ID filmu to 1
+        p_sala_id => 1, -- Zak³adaj¹c, ¿e ID sali to 1
+        p_data_rozpoczecia => TO_DATE('2025-01-25 15:00', 'YYYY-MM-DD HH24:MI')
+    );
+
+    DBMS_OUTPUT.PUT_LINE('Poprawne seanse dodane.');
 END;
 /
 
--- Dodaj test triggera:
-DECLARE 
-    v_rezerwacja_id NUMBER;
+-- Przypadki testuj¹ce b³êdy
 BEGIN
-    -- ZnajdÅº ID istniejÄ…cej rezerwacji
-    SELECT MIN(rezerwacja_id) INTO v_rezerwacja_id 
-    FROM Rezerwacja_table;
-    
-    -- Anuluj rezerwacjÄ™
-    UPDATE Rezerwacja_table
-    SET czy_anulowane = 1
-    WHERE rezerwacja_id = v_rezerwacja_id;
-    
-    -- SprawdÅº czy miejsca zostaÅ‚y zwolnione
-    COMMIT;
-    DBMS_OUTPUT.PUT_LINE('Rezerwacja ' || v_rezerwacja_id || ' zostaÅ‚a anulowana.');
+    -- Przypadek: Seans zaczyna siê przed godzin¹ 07:00
+    admin_seanse.add_seans(
+        p_film_id => 1, -- Zak³adaj¹c, ¿e ID filmu to 1
+        p_sala_id => 1, -- Zak³adaj¹c, ¿e ID sali to 1
+        p_data_rozpoczecia => TO_DATE('2025-01-25 06:00', 'YYYY-MM-DD HH24:MI')
+    );
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('B³¹d: ' || SQLERRM);
 END;
 /
 
-
-        
--- Wyzwalacz na unikalnoï¿½ï¿½ seansï¿½w w sali
-CREATE OR REPLACE TRIGGER ensure_unique_seans_per_sala
-BEFORE INSERT OR UPDATE ON Repertuar_table
-FOR EACH ROW
-DECLARE
-    v_count NUMBER;
 BEGIN
-    -- Sprawdï¿½, czy w tej samej sali i o tej samej godzinie nie ma innego seansu
-    SELECT COUNT(*)
-      INTO v_count
-      FROM Repertuar_table r
-     WHERE r.sala_ref = :NEW.sala_ref
-       AND r.data_rozpoczecia = :NEW.data_rozpoczecia
-       AND r.repertuar_id != :NEW.repertuar_id;
+    -- Przypadek: Seans koliduje z istniej¹cym seansem
+    admin_seanse.add_seans(
+        p_film_id => 1, -- Zak³adaj¹c, ¿e ID filmu to 1
+        p_sala_id => 1, -- Zak³adaj¹c, ¿e ID sali to 1
+        p_data_rozpoczecia => TO_DATE('2025-01-25 16:30', 'YYYY-MM-DD HH24:MI') -- Kolizja z poprzednim seansem
+    );
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('B³¹d: ' || SQLERRM);
+END;
+/
 
-    IF v_count > 0 THEN
-        RAISE_APPLICATION_ERROR(-20005, 'W jednej sali w tym samym czasie moï¿½e byï¿½ tylko jeden seans.');
-    END IF;
+BEGIN
+    -- Przypadek: Próba dodania seansu do nieistniej¹cej sali
+    admin_seanse.add_seans(
+        p_film_id => 1, -- Zak³adaj¹c, ¿e ID filmu to 1
+        p_sala_id => 999, -- Nieistniej¹ce ID sali
+        p_data_rozpoczecia => TO_DATE('2025-01-25 18:00', 'YYYY-MM-DD HH24:MI')
+    );
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('B³¹d: ' || SQLERRM);
+END;
+/
+
+BEGIN
+    -- Przypadek: Próba dodania seansu z nieistniej¹cym filmem
+    admin_seanse.add_seans(
+        p_film_id => 999, -- Nieistniej¹ce ID filmu
+        p_sala_id => 1, -- Zak³adaj¹c, ¿e ID sali to 1
+        p_data_rozpoczecia => TO_DATE('2025-01-25 20:00', 'YYYY-MM-DD HH24:MI')
+    );
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('B³¹d: ' || SQLERRM);
 END;
 /
